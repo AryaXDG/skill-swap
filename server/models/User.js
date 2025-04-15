e87f7a7e9d41eaf158b6a2be6f96c1b4996ff690
Arya Dasgupta <aryadasgupta2004@gmail.com>
2025-04-15 22:01:38 +0530

    feat: Implement password hashing in User model

--- server/models/User.js
+++ server/models/User.js
@@ -1,5 +1,5 @@
 import mongoose from 'mongoose';
-
+import bcrypt from 'bcryptjs';
 
 const userSchema = new mongoose.Schema({
   username: { 
@@ -18,8 +18,25 @@ const userSchema = new mongoose.Schema({
     type: String, 
     required: true 
   },
-
 });
+
+// Pre-save hook to hash password
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('passwordHash')) {
+    return next();
+  }
+  // We use passwordHash as the field to be compatible with a direct 'passwordHash' set
+  // If we were taking a 'password' field, we'd hash that and store it in 'passwordHash'
+  // Assuming the controller will set 'passwordHash' with the plain text pass temporarily
+  const salt = await bcrypt.genSalt(10);
+  this.passwordHash = await bcrypt.hash(this.passwordHash, salt);
+  next();
+});
+
+// Method to compare passwords
+userSchema.methods.matchPassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.passwordHash);
+};
 
 const User = mongoose.model('User', userSchema);
 export default User;
